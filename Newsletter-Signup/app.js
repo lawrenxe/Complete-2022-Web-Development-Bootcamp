c132eb26a019365bfefd095ea4e5a66aa8891cc1
lawrenxe <yizhouw7@student.unimelb.edu.au>
2022-02-17 17:01:29 +0800

    API accomplished

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,9 +2,75 @@ const express = require("express");
 const app = express();
 const request = require("request");
 
-const bodyParser = require("body_parser");
-app.use(bodyParser);
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000.");
-})
+mailchimp.setConfig({
+  apiKey: "a7c673d8dbb2440ecdcfba98a409b6d9-us14",
+  server: 'us14'
+});
+const listID = '8a805c7281';
+
+
+app.use(express.static("public"));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running ");
+});
+
+app.post("/", function(req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+
+    const subscribingUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email
+    }
+
+    async function run() {
+      const response = await mailchimp.lists.addListMember(listID, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastNameb  
+        }
+      }).then(response => {
+        if (response.id !== "") {
+          res.sendFile(__dirname + "/success.html");
+          console.log(response.id);
+        }
+      }).catch(err => {
+        res.sendFile(__dirname + "/fail.html");
+
+        console.log(err);
+      });
+    }
+
+    run();
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+
+
+//API Key
+//a7c673d8dbb2440ecdcfba98a409b6d9-us14
+
+//list ID
+//8a805c7281
